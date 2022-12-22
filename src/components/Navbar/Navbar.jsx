e349a2003dc654aea4194d5407db6b0e913cc834
SHETH MANAV PRASHANT <91129351+Manavsheth78@users.noreply.github.com>
2022-12-22 23:59:44 +0530

    Add files via upload

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,17 +4,17 @@ function Navbar() {
   return (
     <div className='navbar'>
           <div className='logo' >
-          <img src="assets/prakarshlogo.png"  alt="Prakarsh-Logo" />
+          <img src="assets/prakarshlogo2.0.svg"  alt="Prakarsh-Logo" />
         </div>
         <div className='navcontent'>
             <ul className='navicons' > 
               {/* <li className='navitem active'> <a href="#">
                  <img src="assets/user.png" className=' user' alt="" /></a></li> */}
+                 <li>
+                   <button type='button' className='signupbtn'> <a href='#'> SIGN UP </a></button>
+                 </li>
               <li className='navitem'> <a href='#'>
-                 <img src="assets/cart2.png " className='cart' alt="" /></a></li>
-              <li>
-                <button type='button' className='signupbtn'> <a href='#'> SIGN UP </a></button>
-              </li>
+                 <img src="assets/shopcart.svg" className='cart' alt="" /></a></li>
             </ul>
         </div>
   </div>
