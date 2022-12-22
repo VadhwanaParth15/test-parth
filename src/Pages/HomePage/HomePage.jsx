3072022e60269a13e148d380bf80734230d0a226
SHETH MANAV PRASHANT <91129351+Manavsheth78@users.noreply.github.com>
2022-12-22 23:58:09 +0530

    Add files via upload

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,20 +3,24 @@ import './HomePage.css'
 function HomePage() {
   return (
     <div className='HOMEPAGE-MAIN-CONTAINER'>
-        <div className='CONTENT-CONTAINER'>
-            <div className='curved'>
-                <img src="assets/herocurved.png" alt="curved image"/>
-                  
-                    <div className='headercontainer' >
-                       <h1 className='headerline' > Welcome  to <br/> 
-                       <span>PRAKARSH'23</span></h1>
-                    </div>
-                    </div>
-                <div className='heroimage'>
-                   <img src="assets/herosectionimg.svg" className='HOME-CONTENT-SIDE-IMAGE' alt="IMAGE" />
-                 </div>
+      <div className='bgimage'>
+          <img src="assets\hero2.png" alt="bg1"/>
           </div>
-      </div>  
+           <div className='headerline'>
+             <h1> PRAKARSH'<span>23</span></h1>  
+             <h2> ENGAGING mINds, eMpowERiNg sUccEss
+</h2>
+             <p> Prakarsh 23's vision is to assist the rural areas, and villages and to encourage them in the advancement of their lives through technologies developed in Prakarsh by students. </p>
+
+            <button type='button' className='expbtn'><a href='#'>Explore Events <img src="assets/rightarrow.png"></img> </a> </button>
+
+             {/* <div className='launch'>
+             <h1> 1-2 MARCH</h1>
+           </div> */}
+          </div>
+        
+       
+    </div>  
   )
 }
 
